04f0467c2b112ddb178ce09e795905b7e106104b
streamsadmin <streamsadmin@localhost.localdomain>
2018-01-16 17:11:10 +0100

    update cloud-test for IAM

--- tests/cloud-test/test_streaming_analytics.py
+++ tests/cloud-test/test_streaming_analytics.py
@@ -23,19 +23,30 @@ class TestCloud(unittest.TestCase):
         tk.add_toolkit(topo, './test_monitoring')
         tk.add_toolkit(topo, '../../com.ibm.streamsx.monitoring')
 
-    def _get_expected_values(self):
-        expected_vals = []
-        expected_vals.append("TEST_RESULT_PASS")
-        return expected_vals
+    def _get_iam_endpoint(self):
+        iamTokenEndpoint = "" # uses operator default
+        # check if running on test system
+        v2_rest_url = self.credentials.get("v2_rest_url")
+        if "stage1" in v2_rest_url:
+            iamTokenEndpoint = "https://iam.stage1.ng.bluemix.net/oidc/token"
+        return iamTokenEndpoint
 
     def _build_launch_validate(self, name, composite_name):
         topo = Topology(name)
         self._add_toolkits(topo)
 
         # Set up parameters to call the test composite
-        user = self.credentials['userid']
-        password = self.credentials['password']
-        params = {'user':user, 'password':password}
+        user = self.credentials.get("userid")
+        password = self.credentials.get("password")
+        
+        if user is not None:
+            print("Monitor application in the Streaming Analytics service uses user and password")
+            params = {'user':user, 'password':password}
+        else :
+            print("Monitor application in the Streaming Analytics service uses IAM API KEY")
+            iamApiKey = self.credentials.get("apikey") # use value from VCAP_SERVICES file
+            iamTokenEndpoint = self._get_iam_endpoint()
+            params = {'iamApiKey':iamApiKey, 'iamTokenEndpoint':iamTokenEndpoint}
 	
         # Call the test composite
         test_op = op.Source(topo, composite_name, 'tuple<rstring result>', params=params)
@@ -45,6 +56,7 @@ class TestCloud(unittest.TestCase):
         tester.contents(test_op.stream, [{'result':'TEST_RESULT_PASS'}] )
 
         tester.test(self.test_ctxtype, self.test_config)
+
 
     def test_metrics_monitor(self):
         self._build_launch_validate("test_metrics_monitor", "test.metrics::TestMetricsSource")
