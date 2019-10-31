6af7efe51369bb33c9c1bf1fb5fe52855ca315ff
markheger <mark-oliver.heger@de.ibm.com>
2019-10-31 10:10:38 +0000

    update scripts to launch samples

--- samples/launch_job_status_monitor_sample.py
+++ samples/launch_job_status_monitor_sample.py
@@ -4,6 +4,7 @@ from streamsx.topology.topology import *
 from streamsx.topology.context import *
 from streamsx.topology.schema import CommonSchema, StreamSchema
 from streamsx.topology import context
+from streamsx.spl.toolkit import add_toolkit
 import streamsx.spl.op as op
 
 
@@ -12,20 +13,20 @@ sample_toolkit = 'com.ibm.streamsx.monitoring.jobs.sample.JobStatusMonitor'
 
 def _launch(main):
     cfg = {}
-    cfg[streamsx.topology.context.ConfigParams.SSL_VERIFY] = False
-    rc = streamsx.topology.context.submit('DISTRIBUTED', main, cfg)
+    cfg[context.ConfigParams.SSL_VERIFY] = False
+    rc = context.submit('DISTRIBUTED', main, cfg)
 
 def monitor_app():
     topo = Topology('JobStatusMonitorSample')
-    streamsx.spl.toolkit.add_toolkit(topo, monitoring_toolkit)
-    streamsx.spl.toolkit.add_toolkit(topo, sample_toolkit)
+    add_toolkit(topo, monitoring_toolkit)
+    add_toolkit(topo, sample_toolkit)
     r = op.main_composite(kind='com.ibm.streamsx.monitoring.jobs.sample.JobStatusMonitor::Monitor', toolkits=[monitoring_toolkit,sample_toolkit])
     _launch(r[0])
 
 def sample_app():
     topo = Topology('CrashSample')
-    streamsx.spl.toolkit.add_toolkit(topo, monitoring_toolkit)
-    streamsx.spl.toolkit.add_toolkit(topo, sample_toolkit)
+    add_toolkit(topo, monitoring_toolkit)
+    add_toolkit(topo, sample_toolkit)
     r = op.main_composite(kind='com.ibm.streamsx.monitoring.jobs.sample.JobStatusMonitor::SampleJob', toolkits=[monitoring_toolkit,sample_toolkit])
     _launch(r[0])
 
--- samples/launch_system_monitor_sample.py
+++ samples/launch_system_monitor_sample.py
@@ -4,6 +4,7 @@ from streamsx.topology.topology import *
 from streamsx.topology.context import *
 from streamsx.topology.schema import CommonSchema, StreamSchema
 from streamsx.topology import context
+from streamsx.spl.toolkit import add_toolkit
 import streamsx.spl.op as op
 
 
@@ -12,13 +13,13 @@ sample_toolkit = 'com.ibm.streamsx.monitoring.system.sample.SystemMonitorSource'
 
 def _launch(main):
     cfg = {}
-    cfg[streamsx.topology.context.ConfigParams.SSL_VERIFY] = False
-    rc = streamsx.topology.context.submit('DISTRIBUTED', main, cfg)
+    cfg[context.ConfigParams.SSL_VERIFY] = False
+    rc = context.submit('DISTRIBUTED', main, cfg)
 
 def sample_app():
     topo = Topology('SystemMonitorSample')
-    streamsx.spl.toolkit.add_toolkit(topo, monitoring_toolkit)
-    streamsx.spl.toolkit.add_toolkit(topo, sample_toolkit)
+    add_toolkit(topo, monitoring_toolkit)
+    add_toolkit(topo, sample_toolkit)
     r = op.main_composite(kind='com.ibm.streamsx.monitoring.system.sample.SystemMonitorSource::Monitor', toolkits=[monitoring_toolkit,sample_toolkit])
     _launch(r[0])
 
